f55607fb3306124d672cf6e0a8eb543c49a3e2a7
Chris Porter <github@crazysnailboy.net>
2017-05-02 12:00:54 +0100

    Imported Impelon's v1.5 changes

--- src/main/java/de/impelon/disenchanter/DisenchanterMain.java
+++ src/main/java/de/impelon/disenchanter/DisenchanterMain.java
@@ -1,40 +1,42 @@
 package de.impelon.disenchanter;
 
+import de.impelon.disenchanter.proxies.CommonProxy;
+import de.impelon.disenchanter.update.VersionChecker;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import de.impelon.disenchanter.DisenchanterMain;
-import de.impelon.disenchanter.proxies.CommonProxy;
-import de.impelon.disenchanter.update.VersionChecker;
 
-@Mod(modid="disenchanter", name="Disenchanter", version="1.4")
+@Mod(modid="disenchanter", name="Disenchanter", version="1.5")
 public class DisenchanterMain {
-	
+
 	public static final String MODID = "disenchanter";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.5";
+	public static final String PREFIX = EnumChatFormatting.GRAY + "[" + EnumChatFormatting.GOLD + EnumChatFormatting.BOLD +
+			"Disenchanter" + EnumChatFormatting.GRAY + "] " + EnumChatFormatting.RESET;
 	public static final VersionChecker versionChecker = new VersionChecker();
 	public static Configuration config;
-	
+
 	@Mod.Instance(value="disenchanter")
 	public static DisenchanterMain instance;
-	
+
 	@SidedProxy(clientSide="de.impelon.disenchanter.proxies.CombinedClientProxy", serverSide="de.impelon.disenchanter.proxies.ServerProxy")
 	public static CommonProxy proxy;
-	
+
 	@EventHandler
 	public static void preInit(FMLPreInitializationEvent ev) {
 		proxy.preInit(ev);
 	}
-	
+
 	@EventHandler
 	public static void load(FMLInitializationEvent ev) {
 		proxy.load(ev);
 	}
-	
+
 	@EventHandler
 	public static void postInit(FMLPostInitializationEvent ev) {
 		proxy.postInit(ev);
--- src/main/java/de/impelon/disenchanter/proxies/CombinedClientProxy.java
+++ src/main/java/de/impelon/disenchanter/proxies/CombinedClientProxy.java
@@ -1,7 +1,10 @@
 package de.impelon.disenchanter.proxies;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.RenderItem;
+import de.impelon.disenchanter.DisenchanterMain;
+import de.impelon.disenchanter.blocks.TileEntityDisenchantmentTable;
+import de.impelon.disenchanter.gui.TileEntityDisenchantmentTableRenderer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.statemap.DefaultStateMapper;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
@@ -9,30 +12,32 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import de.impelon.disenchanter.blocks.BlockDisenchantmentTable;
-import de.impelon.disenchanter.blocks.TileEntityDisenchantmentTable;
-import de.impelon.disenchanter.gui.TileEntityDisenchantmentTableRenderer;
-import de.impelon.disenchanter.DisenchanterMain;
 
 public class CombinedClientProxy extends CommonProxy {
-	
+
 	@Override
 	public void preInit(FMLPreInitializationEvent ev) {
 		super.preInit(ev);
+
+		ModelLoader.setCustomStateMapper(disenchantmentTable, new DefaultStateMapper() {
+			@Override
+			protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+		        return new ModelResourceLocation(disenchantmentTable.getUnlocalizedName().substring(5), state.toString().split("[\\[\\]]")[1]);
+		    }
+		});
+
+		for (byte meta = 0; meta < 8; meta++)
+			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(disenchantmentTable), meta,
+					new ModelResourceLocation(disenchantmentTable.getUnlocalizedName().substring(5),
+							disenchantmentTable.getStateFromMeta(meta).toString().split("[\\[\\]]")[1]));
 	}
-	
+
 	@Override
 	public void load(FMLInitializationEvent ev) {
 		super.load(ev);
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityDisenchantmentTable.class, new TileEntityDisenchantmentTableRenderer());
-		
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(
-				Item.getItemFromBlock(disenchantmentTable), 0, new ModelResourceLocation(DisenchanterMain.MODID + ":" + disenchantmentTable.getUnlocalizedName().substring(5), "inventory"));
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(
-				Item.getItemFromBlock(disenchantmentTable), 1, new ModelResourceLocation(DisenchanterMain.MODID + ":" + disenchantmentTable.getUnlocalizedName().substring(5), "inventory"));
-		
 	}
-	
+
 	@Override
 	public void postInit(FMLPostInitializationEvent ev) {
 		super.postInit(ev);
