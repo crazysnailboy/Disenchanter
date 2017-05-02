f7d49a1e5ccce43a90a06f7e6bb9b3a86fbf4b0b
Chris Porter <github@crazysnailboy.net>
2017-05-02 11:59:22 +0100

    Imported Impelon's v1.5 changes

--- src/main/java/de/impelon/disenchanter/DisenchanterMain.java
+++ src/main/java/de/impelon/disenchanter/DisenchanterMain.java
@@ -1,20 +1,22 @@
 package de.impelon.disenchanter;
 
-import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import de.impelon.disenchanter.proxies.CommonProxy;
 import de.impelon.disenchanter.update.VersionChecker;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.common.config.Configuration;
 
-@Mod(modid = "DisenchanterMod", name = "Disenchanter", version = "1.4")
+@Mod(modid = "DisenchanterMod", name = "Disenchanter", version = "1.5")
 public class DisenchanterMain {
 
 	public static final String MODID = "DisenchanterMod";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.5";
+	public static final String PREFIX = EnumChatFormatting.GRAY + "[" + EnumChatFormatting.GOLD + EnumChatFormatting.BOLD + "Disenchanter" + EnumChatFormatting.GRAY + "] " + EnumChatFormatting.RESET;
 	public static final VersionChecker versionChecker = new VersionChecker();
 	public static Configuration config;
 
